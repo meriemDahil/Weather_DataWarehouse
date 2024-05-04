ed437a82990cc907ac62113faad6733bd8c85d07
meriem-da <meriem.dahil@slick-pay.com>
2024-05-04 11:40:55 +0200

    Second commit

--- test.py
+++ test.py
@@ -31,24 +31,65 @@ with open(csv_file, 'r') as file:
 conn.commit()
 cursor.close()
 conn.close()
-
 """
 
 
-file_paths = ['Weather_1920-1959_TUNISIA.csv', 'Weather_1960-1989_TUNISIA.csv','Weather_1990-2019_TUNISIA.csv','Weather_2020-2022_TUNISIA.csv']
-dataframes = [pd.read_csv(file) for file in file_paths]
+file_paths = ['Weather_1920-1959_MOROCCO.csv', 'Weather_1960-1989_MOROCCO.csv','Weather_1990-2019_MOROCCO.csv',
+              'Weather_2020-2022_MOROCCO.csv','Weather_1920-1959_TUNISIA.csv','Weather_1960-1989_TUNISIA.csv',
+              'Weather_1990-2019_TUNISIA.csv','Weather_2020-2022_TUNISIA.csv']
+
+dataframes = [pd.read_csv(file,low_memory=False) for file in file_paths]
+
+dataframe_copy=dataframes
+
+file_paths_Morocco=['Weather_1920-1959_MOROCCO.csv', 'Weather_1960-1989_MOROCCO.csv',
+                    'Weather_1990-2019_MOROCCO.csv','Weather_2020-2022_MOROCCO.csv']
+
+file_paths_Tunisia=['Weather_1920-1959_TUNISIA.csv','Weather_1960-1989_TUNISIA.csv',
+                    'Weather_1990-2019_TUNISIA.csv','Weather_2020-2022_TUNISIA.csv']
+
+dataframes_Morocco = [pd.read_csv(file,low_memory=False) for file in file_paths_Morocco]
+dataframes_Tunisia = [pd.read_csv(file,low_memory=False) for file in file_paths_Tunisia]
+
+dataframes_Morocco_copy=dataframes_Morocco.copy()
+dataframes_Tunisia_copy=dataframes_Tunisia.copy()
+
+for i, df in enumerate(dataframe_copy):
+    
+    common_columns = set.intersection(*(set(df.columns) for df in dataframe_copy))
 
 
+for df in dataframe_copy:
+
+    columns_to_drop = [col for col in df.columns if col not in common_columns]
+    if columns_to_drop:
+        df.drop(columns=columns_to_drop, inplace=True)
+
+    print(columns_to_drop)
+    print(df.shape)
+
+
+for i, df in enumerate(dataframe_copy):
+    print(f"DataFrame {i+1} shape:")
+    print(df.shape) 
+    # print(f"DataFrame {i+1} Info:")
+    # print(df.info())  
+    print("\n")
+    #non_null_counts = df.notna().sum()
+    #print(non_null_counts)
+    print("\n")
+
+"""
 for i, df in enumerate(dataframes):
     print(f"DataFrame {i+1} shape:")
     print(df.shape) 
     print(f"DataFrame {i+1} Info:")
     print(df.info())  
-    
     print("\n")
     non_null_counts = df.notna().sum()
     print(non_null_counts)
     print("\n")
+
 
 for i, df in enumerate(dataframes):
     pd.set_option('display.max_columns', None)  
@@ -57,20 +98,5 @@ for i, df in enumerate(dataframes):
     print(df.head())
     print("\n")
 
-print("\n")
-
-
-for i, df in enumerate(dataframes):
-    
-    columns_with_only_null = df.columns[df.isna().sum() > 70000]
-    print(columns_with_only_null)
-    print("\n")
-
-
-threshold = 1000  
-
-non_null_counts = df.notna().sum()
-
-columns_drop = non_null_counts[non_null_counts < threshold].index
-df_filled = df.drop(columns=columns_drop)
+"""
 
